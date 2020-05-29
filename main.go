73b78c0b991d365758f6578d1a65644460b6f845
杨金元 <yangjinyuan@nationsky.com>
2020-05-29 18:13:01 +0800

    this is my first commit for the github

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
 
+func Sayhello(writer http.ResponseWriter, request *http.Request) {
 
-
-
-
-
-
-
+}
 
 func main() {
-	
-	
+	http.HandleFunc("/", Sayhello)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		fmt.Println("Http Service start failed!")
+		return
+	}
+
 }
